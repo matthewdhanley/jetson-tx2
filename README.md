ddfccd00063642fc748416ca8376fbed7008d026
matthewdhanley <mattdhanley@gmail.com>
2018-02-14 04:17:39 +0000

    added matrix mult

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # jetson-tx2
 Independent study at the University of Colorado Boulder about the NVIDIA Jetson TX2
+To install this project onto your linux machine, use git
+Simply type into the command line:
+```
+git clone https://github.com/matthewdhanley/jetson-tx2.git
+```
+
+Once it's there, you can run the following command to update it within the jetson-tx2 directory:
+```
+git pull origin master
+```
+
+That's it!
--- cuda/intro/mat_mult.cu
+++ cuda/intro/mat_mult.cu
@@ -21,6 +21,7 @@ void native_mat_mult(int *a, int *b, int *result, int m, int n, int k)
 
 }
 
+// To be run on the GPU, hence the __global__ tag
 __global__ void gpu_matrix_mult(int *a, int *b, int *c, int m, int n, int k)
 {
     int row = blockIdx.y * blockDim.y + threadIdx.y; // get the row
@@ -39,9 +40,20 @@ __global__ void gpu_matrix_mult(int *a, int *b, int *c, int m, int n, int k)
 
 int main(int argc, char const *argv[])
 {
-	int m, n, k;
-	printf("Input m n k::\n");
-	scanf("%d %d %d", &m, &n, &k);
+	int m, n, k; // init matrix dimensions
+    printf("---------------------------------------------\n");
+    printf("We will be multiplying two matrices\n");
+    printf("The first will be of size m x n\n");
+    printf("The second will be of size n x k\n");
+    printf("I will have you choose these dimensions!\n");
+    printf("---------------------------------------------\n\n");
+    printf("Input m:\n");
+	scanf("%d", &m);
+    printf("\nInput n:\n");
+    scanf("%d", &n);
+    printf("\nInput k:\n");
+    scanf("%d", &k);
+    printf("\n");
 
 
     // Initialize pointers
@@ -83,6 +95,22 @@ int main(int argc, char const *argv[])
 
     // ================================ GPU =========================================
 
+    // the final matrix will have size m x k
+    // we need to spawn enough threads to compute all of the entries
+
+    unsigned int grid_rows = (m + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    unsigned int grid_cols = (k + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
+    printf("---------------------------------------------\n");
+    printf("The resulting matrix will be of size %d x %d\n", m, k);
+    printf("\nI am launching a grid size of %d x %d blocks\n", grid_rows, grid_cols);
+    printf("Each block will be %d x %d threads\n",BLOCK_SIZE,BLOCK_SIZE);
+    printf("This will give you %d x %d available threads\n",grid_rows*BLOCK_SIZE,grid_cols*BLOCK_SIZE);
+    printf("---------------------------------------------\n\n");
+    printf("Press ENTER to begin computation on GPU...\n");
+    getchar();
+    getchar();
+
     // start to count execution time of GPU version
     cudaEventRecord(start, 0);
 
@@ -96,15 +124,16 @@ int main(int argc, char const *argv[])
     cudaMemcpy(gpu_a, gpu_a, sizeof(int)*m*n, cudaMemcpyHostToDevice);
     cudaMemcpy(gpu_b, gpu_b, sizeof(int)*n*k, cudaMemcpyHostToDevice);
 
-    unsigned int grid_rows = (m + BLOCK_SIZE - 1) / BLOCK_SIZE;
-    unsigned int grid_cols = (k + BLOCK_SIZE - 1) / BLOCK_SIZE;
     dim3 dimGrid(grid_cols, grid_rows);
     dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
 
     // Launch kernel
+    // Kernels will always be launched using triple brackets
+    // the first input in the triple brackets is the dimension of the grid
+    // the second is the dimension of the block
     gpu_matrix_mult<<<dimGrid, dimBlock>>>(gpu_a, gpu_b, gpu_c, m, n, k);
 
-    // Transefr results from device to host
+    // Transefer results from device to host
     cudaMemcpy(cpu_c, gpu_c, sizeof(int)*m*k, cudaMemcpyDeviceToHost);
     cudaThreadSynchronize();
 
@@ -115,6 +144,9 @@ int main(int argc, char const *argv[])
     // compute time elapse on GPU computing
     cudaEventElapsedTime(&gpu_time, start, stop);
     printf("Time elapsed on matrix multiplication of %dx%d * %dx%d on GPU: %f ms.\n\n", m, n, n, k, gpu_time);
+
+    printf("Press ENTER to begin computation on CPU...\n");
+    getchar();
 
     // cpu - start recording time at 0
 	cudaEventRecord(start, 0);
