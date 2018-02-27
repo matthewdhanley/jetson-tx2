dbc506b6d54cfe3c5ea0cc54b130a969d0045555
maha2895 <maha2895@colorado.edu>
2018-02-26 20:38:44 -0700

    updated git.sh

--- git.sh
+++ git.sh
@@ -2,7 +2,7 @@
 echo 'This will overwrite any local changes you have made.'
 echo 'Continue? (y/n)'
 read answer
-if [ "$answer" -eq "y" ]
+if [ "$answer" == "y" ]
 then
 	git fetch --all
 	git reset --hard origin/master
